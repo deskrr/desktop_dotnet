507892d77cc68f8bb71025f55c4560a9d92270f2
Fahim <faztp12@gmail.com>
2019-07-04 16:33:17 +0530

    Track User Process with timer

---
+++ desktop_dotnet/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using desktop_dotnet.win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace desktop_dotnet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public int a = 2;
+        public BehaviourMonitor monitor = new BehaviourMonitor();
+
+        public List<UserActivity> activities = new List<UserActivity>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.updateActiveProcess();
+
+            listView.ItemsSource = this.activities;
+        }
+
+        async void updateActiveProcess()
+        {
+            while (true)
+            {
+                string activeProcess = "";
+                await Task.Delay(1000);
+                try
+                {
+                    activeProcess = this.monitor.GetActiveProcessFileName();
+                    using (Icon ico = this.monitor.getIcon(activeProcess))
+                    {
+                        image.Source = Imaging.CreateBitmapSourceFromHIcon(ico.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    }
+                    this.getActivity(activeProcess).addTime(1);
+                } catch (Exception e)
+                {
+                    activeProcess = "error";
+                    debugText.Text += e.Message;
+                    debugText.Text += e.StackTrace;
+                    Console.WriteLine(e.StackTrace);
+                }
+                labl.Content = System.IO.Path.GetFileName(activeProcess);
+            }
+        }
+
+        UserActivity getActivity(string path)
+        {
+            string filename = System.IO.Path.GetFileName(path);
+            UserActivity activity = this.activities.FirstOrDefault((a) => a.fileName == filename);
+            if (activity == null)
+            {
+                activity = new UserActivity();
+                activity.fileName = filename;
+                using (Icon ico = this.monitor.getIcon(path))
+                {
+                    activity.icon = Imaging.CreateBitmapSourceFromHIcon(ico.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                }
+                this.activities.Add(activity);
+                listView.ItemsSource = null;
+                listView.ItemsSource = this.activities;
+            }
+            return activity;
+        }
+    }
+
+    public class UserActivity : INotifyPropertyChanged
+    {
+        public string fileName { get; set; }
+        public ImageSource icon { get; set; }
+        public TimeSpan duration { get; set ; }
+        public string durationFormatted { get { return this.duration.ToString(); } }
+
+        public UserActivity()
+        {
+            this.duration = new TimeSpan();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void addTime(int seconds)
+        {
+            this.duration = this.duration.Add(new TimeSpan((long)(seconds * Math.Pow(10, 7))));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("durationFormatted"));
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ desktop_dotnet/win32/BehaviourMonitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktop_dotnet.win32
+{
+    public class BehaviourMonitor
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        public string GetActiveProcessFileName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+            return p.MainModule.FileName;
+        }
+
+        public Icon getIcon(string path)
+        {
+            return Icon.ExtractAssociatedIcon(path);
+        }
+    }
+}
